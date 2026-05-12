c932b38ea8d390f307b44f93d3720915a087db52
lucas Garcia <lucastobiasg@gmail.com>
2026-05-11 19:00:11 -0500

    app.jsx and main.jsx built

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Pages (you will create these files)
+import BookList from "./pages/BookList";
+import BookDetails from "./pages/BookDetails";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Account from "./pages/Account";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        {/* Redirect root → /books */}
+        <Route path="/" element={<Navigate to="/books" replace />} />
+
+        {/* All books */}
+        <Route path="/books" element={<BookList />} />
+
+        {/* Single book */}
+        <Route path="/books/:id" element={<BookDetails />} />
+
+        {/* Auth */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Account */}
+        <Route path="/account" element={<Account />} />
+
+        {/* Catch-all */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </>
+  );
 }
